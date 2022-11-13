dca2d58b1b6cfaff6ff427b8b983ca5605fbc401
Kacper Koi Marciniak <kacper.d3veloper@gmail.com>
2022-11-13 11:00:03 +0100

    fixes and cleaning

--- app.js
+++ app.js
@@ -1,59 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const path = require('path');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname,'public')));
+
+const tasks = [];
+app.post("/add-task", function(req, res){
+    const taskName = req.body.taskName;
+    const content = req.body.taskDescription;
+
+    let taskItem = {
+        name: taskName,
+        description: content
+    };
+    tasks.push(taskItem); 
+    console.log(taskItem);
+
+    res.redirect("/");
+});
 
 app.get("/", function(req, res){
-
     res.sendFile(__dirname + "/views/todo.ejs");
 
-    //There is no work without some coffee and jellys
-    let coffeeBeforeWork = {
-        name: 'Drink coffee',
-        description: 'Remember to drink coffee before getting to work!'
-    };
-    let jellysBeforeWork = {
-        name: 'Eat Jellys',
-        description: 'Remember to eat yummy jellys before getting to work!'
-    };
-
-    //tasks
-    let tasks = [coffeeBeforeWork, jellysBeforeWork];
     for (let i = 0; i < tasks.length; i++) {
         var checkBox = req.body.checkBox + i;
     }
 
-    //add task
-    app.post("/add-task", function(req, res){
-        let taskName = req.body.taskName;
-        let content = req.body.taskDescription;
-
-        let taskItem = {
-            name: taskName,
-            description: content
-        };
-        tasks.push(taskItem); 
-        console.log(taskItem);
-
-        res.redirect("/app");
+    res.render('todo', {
+        tasks: tasks,
+        checkBox: checkBox
     });
-
-    //clear tasks
-
-    //app
-    app.get("/app", function(req, res){
-        res.render('todo', {
-            tasks: tasks,
-            checkBox: checkBox
-        });
-    });
-
-    res.redirect("/app");
-
 });
 
 app.listen(5000, () => {
